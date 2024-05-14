7247f16c71372c7584da9e30f03b5f7ba6adab56
robot <kokokyaw.dev@gmail.com>
2024-05-15 00:23:41 +0630

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<script setup>
+import TheWelcome from '@/components/TheWelcome.vue'
+import { ref } from 'vue';
+
+let message = ref("hello world");
+
+setTimeout(() => {
+  message.value = "I have been Changed";
+}, 2000)
+
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+
+    <p>{{ message }}</p>
+
+    <p>
+      <input type="text" v-model="message" />
+    </p>
+    
+  </main>
+</template>
